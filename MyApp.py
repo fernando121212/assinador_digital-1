371881767ba7207b4c12c0da90d56e97321e5799
Sangiorgio <35496140+fernando121212@users.noreply.github.com>
2018-11-10 01:54:18 -0200

    Alteração na msg para boa noite
    Alteração na msg para boa noite

--- MyApp.py
+++ MyApp.py
@@ -4,7 +4,7 @@ from kivy.uix.button import Button
 class MyApp(App):
 
     def build(self):
-        return Button(text='Olá Mundo!')
+        return Button(text='Boa noite!')
 
 
 if __name__ == '__main__':
